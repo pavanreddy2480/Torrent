0766c33465e9cd5b74741c9c9eceefa91f96744f
rick <pavanreddy2480@gmail.com>
2025-09-11 22:44:32 +0530

    added the client and tracker template

---
+++ client/client.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+using namespace std;
+
+int main(){
+    string ip_addr;
+    int port;
+    cin >> ip_addr>>port;
+
+    int cSock=socket(AF_INET,SOCK_STREAM,0);
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(port);
+
+    inet_pton(AF_INET,ip_addr.c_str(),&serverAddr.sin_addr);
+
+    connect(cSock,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+
+
+    string msg="hello from the other side.";
+    send(cSock,msg.c_str(),msg.size(),0);
+
+    close(cSock);
+    return 0;
+}
