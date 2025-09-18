7ad899343cc0c736fcd3d2f919ebfee9f74ff432
rick <pavanreddy2480@gmail.com>
2025-09-18 22:35:02 +0530

    interm submission

--- tracker/tracker1.cpp
+++ tracker/tracker1.cpp
@@ -1,31 +1,23 @@
-// tracker1.cpp  -- Primary tracker that also forwards updates to Tracker2
-
-#include "tracker_common.hpp"   // see below
-
-int main(int argc,char**argv){
-    if(argc!=5){
-        cerr<<"Usage: "<<argv[0]
-            <<" <bind_ip> <bind_port> <peer_ip> <peer_port>\n";
+#include "tracker_common.hpp"
+int main(){
+    string bind_ip,peer_ip;
+    int bind_port,peer_port;
+    if(!(cin>>bind_ip>>bind_port>>peer_ip>>peer_port)){
+        cerr<<"Invalid input format\n";
         return 1;
     }
-    string bind_ip  = argv[1];
-    int    bind_port= stoi(argv[2]);
-    string peer_ip  = argv[3];
-    int    peer_port= stoi(argv[4]);
-
     Tracker tracker;
-    SyncQueue sync(peer_ip,peer_port);     // <== Forwarder is active in tracker1
-    thread retrier(&SyncQueue::run,&sync); retrier.detach();
-
-    int listenFd = prepare_listener(bind_ip,bind_port);
+    SyncQueue sync(peer_ip,peer_port);
+    thread retrier(&SyncQueue::run,&sync);
+    retrier.detach();
+    int listenFd=prepare_listener(bind_ip,bind_port);
     cerr<<"Tracker1 listening on "<<bind_ip<<":"<<bind_port
-        <<"  forwarding to "<<peer_ip<<":"<<peer_port<<"\n";
-
+        <<" forwarding to "<<peer_ip<<":"<<peer_port<<"\n";
     while(true){
         sockaddr_in cli; socklen_t clen=sizeof(cli);
-        int cfd = accept(listenFd,(sockaddr*)&cli,&clen);
-        if(cfd<0){ if(errno==EINTR) continue; perror("accept"); break; }
-        thread t(sessionWorker,cfd,ref(tracker),&sync,true); // forward=true
+        int cfd=accept(listenFd,(sockaddr*)&cli,&clen);
+        if(cfd<0){if(errno==EINTR)continue;perror("accept");break;}
+        thread t(sessionWorker,cfd,ref(tracker),&sync,true);
         t.detach();
     }
     close(listenFd);
--- tracker/tracker2.cpp
+++ tracker/tracker2.cpp
@@ -1,29 +1,20 @@
-// tracker2.cpp  -- Secondary tracker that stays in sync but never forwards
-
-#include "tracker_common.hpp"   // see below
-
-int main(int argc,char**argv){
-    if(argc!=5){
-        cerr<<"Usage: "<<argv[0]
-            <<" <bind_ip> <bind_port> <peer_ip> <peer_port>\n";
+#include "tracker_common.hpp"
+int main(){
+    string bind_ip,peer_ip;
+    int bind_port,peer_port;
+    if(!(cin>>peer_ip>>peer_port>>bind_ip>>bind_port)){
+        cerr<<"Invalid input format\n";
         return 1;
     }
-    string bind_ip  = argv[1];
-    int    bind_port= stoi(argv[2]);
-    string peer_ip  = argv[3];    // still passed for symmetry
-    int    peer_port= stoi(argv[4]);
-
     Tracker tracker;
-    SyncQueue dummy(peer_ip,peer_port);    // created but never used
-    int listenFd = prepare_listener(bind_ip,bind_port);
+    SyncQueue dummy(peer_ip,peer_port); // created but not used for forwarding
+    int listenFd=prepare_listener(bind_ip,bind_port);
     cerr<<"Tracker2 listening on "<<bind_ip<<":"<<bind_port
-        <<"  peer="<<peer_ip<<":"<<peer_port<<"\n";
-
+        <<" peer="<<peer_ip<<":"<<peer_port<<"\n";
     while(true){
         sockaddr_in cli; socklen_t clen=sizeof(cli);
-        int cfd = accept(listenFd,(sockaddr*)&cli,&clen);
-        if(cfd<0){ if(errno==EINTR) continue; perror("accept"); break; }
-        // forward flag = false so we do NOT push to peer
+        int cfd=accept(listenFd,(sockaddr*)&cli,&clen);
+        if(cfd<0){if(errno==EINTR)continue;perror("accept");break;}
         thread t(sessionWorker,cfd,ref(tracker),&dummy,false);
         t.detach();
     }
