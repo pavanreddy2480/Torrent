7c2d19637ee78f6a44c2fdcc740874ad6ae4868e
rick <pavanreddy2480@gmail.com>
2025-09-27 18:32:10 +0530

    interm

--- 2025201049_A3_interim/tracker1.cpp
+++ 2025201049_A3_interim/tracker1.cpp
@@ -2,20 +2,22 @@
 int main(){
     string bind_ip,peer_ip;
     int bind_port,peer_port;
-    if(!(cin>>peer_ip>>peer_port>>bind_ip>>bind_port)){
+    if(!(cin>>bind_ip>>bind_port>>peer_ip>>peer_port)){
         cerr<<"Invalid input format\n";
         return 1;
     }
     Tracker tracker;
-    SyncQueue dummy(peer_ip,peer_port);
+    SyncQueue sync(peer_ip,peer_port);
+    thread retrier(&SyncQueue::run,&sync);
+    retrier.detach();
     int listenFd=prepare_listener(bind_ip,bind_port);
-    cerr<<"Tracker2 listening on "<<bind_ip<<":"<<bind_port
-        <<" peer="<<peer_ip<<":"<<peer_port<<"\n";
+    cerr<<"Tracker1 listening on "<<bind_ip<<":"<<bind_port
+        <<" forwarding to "<<peer_ip<<":"<<peer_port<<"\n";
     while(true){
         sockaddr_in cli; socklen_t clen=sizeof(cli);
         int cfd=accept(listenFd,(sockaddr*)&cli,&clen);
         if(cfd<0){if(errno==EINTR)continue;perror("accept");break;}
-        thread t(sessionWorker,cfd,ref(tracker),&dummy,false);
+        thread t(sessionWorker,cfd,ref(tracker),&sync,true);
         t.detach();
     }
     close(listenFd);
